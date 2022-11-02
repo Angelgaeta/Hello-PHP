6b32ca063c8b42fd176ed3d58504f73db15937bd
Angel R <93774378+Angelgaeta@users.noreply.github.com>
2022-11-02 21:31:44 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,6 @@
 <?php 
-  echo <<<_END
+
+  /*echo <<<_END
     <html><head><title>Formulaire de téléversement en PHP</title></head><body>
     <form method='post' action='upload.php' enctype='multipart/form-data'>
     Sélectionnez le fichier : <input type='file' name='nomfichier' size='10'>
@@ -13,5 +14,41 @@ _END;
     move_uploaded_file($_FILES['nomfichier']['tmp_name'], $nom);
     echo "Image téléchargée : '$nom'<br><img src='$nom'>";
   }
-  echo "</body></html>";
+  echo "</body></html>";*/
+
+
+/********************************VERSION SECURISÉ*********************************/
+echo <<<_END
+<html><head><title>Formulaire de téléversement en PHP</title></head><body>
+<form method='post' action='upload.php' enctype='multipart/form-data'>
+Sélectionnez un fichier JPG, GIF, PNG ou TIF :
+<input type='file' name='nomfichier' size='10'>
+<input type='submit' value='Déposer'></form>
+_END;
+
+if ($_FILES)
+{
+$nom = $_FILES['nomfichier']['name'];
+
+switch($_FILES['nomfichier']['type'])
+{
+  case 'image/jpeg': $ext = 'jpg'; break;
+  case 'image/gif':  $ext = 'gif'; break;
+  case 'image/png':  $ext = 'png'; break;
+  case 'image/tiff': $ext = 'tif'; break;
+  default:           $ext = '';    break;
+}
+if ($ext)
+{
+  $n = "image.$ext";
+  move_uploaded_file($_FILES['nomfichier']['tmp_name'], $n);
+  echo "Image téléchargée : '$nom' de type '$n':<br>";
+  echo "<img src='$n'>";
+}
+else echo "'$nom' n'est pas accepté comme fichier image";
+}
+else echo "Aucune image chargée";
+
+echo "</body></html>";
+
 ?>
